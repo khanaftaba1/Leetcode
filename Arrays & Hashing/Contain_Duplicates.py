6d45df3be58555f2d9b945af255cb902951e802e
Aftab Khan <khanaftaba1@gmail.com>
2024-12-19 14:49:37 -0500

    new solution added

--- Arrays & Hashing/Contain_Duplicates.py
+++ Arrays & Hashing/Contain_Duplicates.py
@@ -23,3 +23,21 @@ class Solution:
 #                 return True
 #             hashset.add(n)
 #         return False
+
+
+#### Other solution
+
+# numbers = [1, 2, 3, 4]
+
+# def duplicates(nums):
+    
+#     dic = {}
+    
+#     for i in nums:
+#         dic[i] = dic.get(i, 0) + 1
+#         if dic[i]>1:
+#             return True
+    
+#     return False
+
+# print(duplicates(numbers))
